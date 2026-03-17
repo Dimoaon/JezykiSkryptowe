5238b719b7ef7ba9e651bf058d8066c553c27f76
Dzmitry Skavardka <skovdimka@gmail.com>
2026-03-17 19:29:21 +0100

    implement parser

--- lab2/lab_2.py
+++ lab2/lab_2.py
@@ -1,10 +1,15 @@
 import sys
+from core.parser import process_sentences
 from core.cleaner import clean_text
+
+def print_sentence(sentence):
+    print(">>", sentence)
 
 def main():
     text = sys.stdin.read()
     cleaned = clean_text(text)
-    print(cleaned)
+
+    process_sentences(cleaned, print_sentence)
 
 if __name__ == "__main__":
     main()
