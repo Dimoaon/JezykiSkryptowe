d5e2e8883c41f0a1d9eb9bdff776716043b68524
Dzmitry Skavardka <skovdimka@gmail.com>
2026-03-17 19:52:38 +0100

    add example processing function (count_sentences)

--- lab2/lab_2.py
+++ lab2/lab_2.py
@@ -1,15 +1,18 @@
 import sys
-from core.parser import process_sentences
 from core.cleaner import clean_text
-
-def print_sentence(sentence):
-    print(">>", sentence)
+from core.parser import process_sentences
+from core.utils import count_sentences
 
 def main():
     text = sys.stdin.read()
+
     cleaned = clean_text(text)
 
-    process_sentences(cleaned, print_sentence)
+    process, result = count_sentences()
+
+    process_sentences(cleaned, process)
+
+    print(result())
 
 if __name__ == "__main__":
     main()
